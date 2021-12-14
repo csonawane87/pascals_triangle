ca7e1f5578dc4f7dc44d215d916772ea063be9b5
csonawane87 <96120626+csonawane87@users.noreply.github.com>
2021-12-14 18:59:29 +0530

    Updated the code

--- Pascal_Triangle.py
+++ Pascal_Triangle.py
@@ -1,13 +1,21 @@
+
 # Print Pascal's Triangle in Python
  
 # input n
 n = input('Index of (a+b): ')
 n = int(n)
  
-# iterarte upto n
-for i in range(n):
-    # adjust space
-    print(' '*(n-i), end='')
+for i in range(1, n+1):
+    for j in range(0, n-i+1):
+        print(' ', end='')
  
-    # compute power of 11
-    print(' '.join(map(str, str(11**i))))
+    # first element is always 1
+    C = 1
+    for j in range(1, i+1):
+ 
+        # first value in a line is always 1
+        print(' ', C, sep='', end='')
+ 
+        # using Binomial Coefficient
+        C = C * (i - j) // j
+    print()
